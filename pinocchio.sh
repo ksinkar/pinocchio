197caea287b0380f345d2a6e213ade7bd0ad4117
Koustubh Sinkar <ksinkar@gmail.com>
2016-06-07 14:56:14 +0530

    Added the install and run subcommands
    1. ./pinocchio install <package_list>
       this helps in installing custom adhoc package lists from
       the pinocchio command line without mucking up the custom
       directory structure
    2. ./pinocchio run <custom_script>
       this helps in running custom bash scripts from the pinocchio
       command line without mucking up the custom directory structure
       And one can also take advantage of  pinocchio's inbuilt functions
       for detecting OS and users.

--- pinocchio.sh
+++ pinocchio.sh
@@ -25,6 +25,13 @@ usage="usage: ./pinocchio [-v] [-h]\n
 To do system operations run the following pinocchio commands as root/sudo.\n
 \t setup \t\t setup a complete system from scratch"
 
+check_root=check_root.sh
+set_package_manager=set_package_manager.sh
+install_packages=install_packages.sh
+prerequisites=install_prerequisites.sh
+custom=custom.sh
+set_users_groups=set_users_groups.sh
+run_puppet=run_puppet.sh
 
 if [[ $1 == "setup" ]]; then
     echo "checking if the user is root"
@@ -34,17 +41,45 @@ if [[ $1 == "setup" ]]; then
         source preload.sh
     fi
     echo "setting up the package manager"
-    source set_package_manager.sh
+    source $set_package_manager
     echo "setting up the install packages function"
-    source install_packages.sh
+    source $install_packages
     echo "installing the prerequisites"
-    source install_prerequisites.sh
+    source $install_prerequisites
     echo "installing the custom dependencies"
-    source custom.sh
+    source $custom
     echo "setting up users and groups"
-    source set_users_groups.sh
+    source $set_users_groups
     echo "running puppet"
-    source run_puppet.sh
+    source $run_puppet
+elif [[ $1 == "install" ]]; then
+    package_list_file=$2
+    if [[ -f $package_list_file ]]; then
+        source $check_root
+        if [[ -f preload.sh ]]; then
+            source $preload
+        fi
+        source $set_package_manager
+        source $install_packages
+        install_packages $package_list_file
+    else
+        echo "File not found"
+        exit 1
+    fi
+elif [[ $1 == "run" ]]; then
+    script_to_be_run=$2
+    if [[ -f $script_to_be_run ]]; then
+        source $check_root
+        if [[ -f preload.sh ]]; then
+            source $preload
+        fi
+        source $set_package_manager
+        source $install_packages
+        source $script_to_be_run
+    else
+        echo "File not found"
+        exit 1
+    fi
 else
     while getopts "hv" opt; do
         case $opt in
